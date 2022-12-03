64942d0ea14b90c6011b8285fa6d164bdbdd2889
LidaPetr <lpetropoulou@homex.com>
2022-12-03 22:06:20 +0000

    Code for day 1

---
+++ 2022/d1.py
@@ -0,0 +1,43 @@
+import time
+
+
+def d1_p1(numbers):
+    count = 0
+    max_calories = 0
+    for number in numbers:
+        if number:
+            count += int(number)
+            if count > max_calories:
+                max_calories = count
+        else:
+            count = 0
+    return max_calories
+
+
+def d1(numbers,top):
+    count = 0
+    calories = []
+    for number in numbers:
+        if number:
+            count += int(number)
+        else:
+            calories.append(count)
+            count = 0
+    if count:
+        calories.append(count)
+    calories.sort(reverse=True)
+    return sum(calories[:top])
+
+
+if __name__ == "__main__":
+    replit_start_time = time.perf_counter()
+    with open("d1.txt") as f:
+        data = f.readlines()
+    numbers = [x.strip() for x in data]
+
+    print(d1(numbers,1))
+    print(d1(numbers,3))
+
+    # Keep this line at the end of your code
+    replit_end_time = time.perf_counter()
+    print("Elapsed time:", replit_end_time - replit_start_time)
